fcef19e72f5b34fe15355ef6e1b8325200e2e5ef
rainfool <rainfool1024@gmail.com>
2017-08-04 21:04:48 +0800

    update add two numbers

--- Number_2/solution.py
+++ Number_2/solution.py
@@ -19,20 +19,29 @@ class ListNode(object):
 class Solution(object):
 
     def addTwoNumbers(self, l1, l2):
-        l1Size = len(l1)
-        l2Size = len(l2)
+        if l1 == None: return l2
+        if l2 == Node: return l1
 
-        loopList
-        if l1Size > l2Size:
-            loopList = l1
-        else
-            loopList = l2
+        flag = 0
+        dummy = ListNode(0)
+        p = dummy
 
-
-        while loop.hasNext():
-            int t = loopNext
-            resultList =
-
+        while l1 and l2:
+            p.next = ListNode((l1.val+l2.val +flag) % 10)
+            flag = (l1.val + l2.val + flag) / 10
+            l1 = l1.next; l2 = l2.next; p = p.next
+        if l2:
+            while l2:
+                p.next = ListNode((l2.val + flag) % 10)
+                flag = (l2.val + flag) /10
+                l2 = l2.next; p = p.next
+        if l1:
+            while l1:
+                p.next = ListNode((l1.val + flag) % 10)
+                flag = (l1.val + flag) /10
+                l1 = l1.next; p = p.next
+        if flag == 1; p.next = ListNode(1)
+        return dummy.next
 
 if __name__ == "__main__":
     main()
