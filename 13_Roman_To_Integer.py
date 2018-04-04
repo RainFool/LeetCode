4ff727d16305a9a84ac33c3270760b6f32af3f0b
RainFool <rainfool1024@gmail.com>
2018-04-04 09:25:31 +0800

    完成13、14

---
+++ 13_Roman_To_Integer.py
@@ -0,0 +1,26 @@
+'''
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+'''
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        numerals = { "M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1 }
+        sum = 0
+        max = 1
+        for in xrange()
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.romanToInt("I")
+    solution.romanToInt("V")
+    solution.romanToInt("X")
+    solution.romanToInt("L")
+    solution.romanToInt("C")
+    solution.romanToInt("D")
+    solution.romanToInt("M")
