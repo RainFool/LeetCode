54926021775590bf007d854654335e5ca9c9853b
RainFool <rainfool1024@gmail.com>
2019-01-12 11:50:36 +0800

    reverse linked list

---
+++ leetcodejvm/src/main/kotlin/linkedlist/LinkedListUtil.kt
@@ -0,0 +1,30 @@
+package linkedlist
+
+import datastructure.ListNode
+
+fun intListToLinkedList(list: List<Int>): ListNode? {
+
+    if (list.isEmpty()) {
+        return null
+    }
+    val dummy = ListNode()
+    var cur: ListNode? = dummy
+
+    list.forEach {
+        cur?.next = ListNode(it)
+        cur = cur?.next
+    }
+    return dummy.next
+}
+
+fun ListNode.print() {
+    val sb = StringBuilder()
+    var cur: ListNode? = this
+    do {
+        sb.append(cur?.`val`)
+        sb.append("->")
+        cur = cur?.next
+    } while (cur != null)
+    sb.append("NULL")
+    println(sb)
+}
