f31d13213f92ba17f936e0057b407c0bf59b7d62
krystian <krystian@tencent.com>
2022-11-29 20:18:44 +0800

    39题临时提交

---
+++ 39.组合总和.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=39 lang=java
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+class Solution {
+
+    // public static final void main(String args[]) {
+    // int[] a = new int[] { 2, 3, 6, 7 };
+    // new Solution39().combinationSum(a, 7);
+    // }
+
+    private List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null) {
+            return ans;
+        }
+        dfs(new ArrayList<>(), candidates, target);
+        return ans;
+    }
+
+    private void dfs(List<Integer> list, int[] candidates, int target) {
+        if (target == 0) {
+            ans.add(list);
+        } else {
+            for (int i = 0; i < candidates.length; i++) {
+                int cur = candidates[i];
+                if (cur <= target) {
+                    list.add(cur);
+                    dfs(list, candidates, target - cur);
+                }
+            }
+        }
+        if (list.size() > 0) {
+            list.remove(list.size() - 1);
+        }
+    }
+}
+// @lc code=end
